3331267788dd79751b4198836d6889d55e0473f8
j h <98184939+okythoos@users.noreply.github.com>
2022-08-09 04:22:47 -0700

    Update project leap year step by step.c

--- project leap year step by step.c
+++ project leap year step by step.c
@@ -29,6 +29,20 @@ int main() {
 - If the year can be evenly divided by 4 then it is a leap year, however…
 - If that year can be evenly divided by 100, and it is not evenly divided by 400, then it is NOT a leap year.
 - If that year is evenly divisible by 400, then it is a leap year.*/
-  
+    }
+  else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+ 
+    std::cout << year;
+    std::cout << " falls on a leap year.\n";
+ 
+  }
+  else {
+ 
+    std::cout << year;
+    std::cout << " is not a leap year.\n";
+ 
+  }
+ 
+}
   
 }
