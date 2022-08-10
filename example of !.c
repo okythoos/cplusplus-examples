3c368c3491327a5d4c86bc3e1d947a0d32098929
j h <98184939+okythoos@users.noreply.github.com>
2022-08-09 18:50:04 -0700

    Create example of !.c

---
+++ example of !.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+  bool logged_in = false;
+  //bool because its true or false
+  
+  // Write the code below:
+  if (!logged_in) {
+    //not logged in
+    std::cout << "Try again!\n";
+    
+  }
+  
+}
