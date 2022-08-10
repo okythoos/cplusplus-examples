fbb05bfae9440a10362f15c7e0b591372f414c40
j h <98184939+okythoos@users.noreply.github.com>
2022-08-09 18:39:36 -0700

    Create example of &&.c

---
+++ example of &&.c
@@ -0,0 +1,17 @@
+int main() {
+  
+  int hunger = true;
+  int anger = true;
+  
+// then code below
+  
+  if (hunger && anger) {
+
+    std::cout << "Hangry\n";
+
+  }
+
+  
+}
+
+// this is, if hunger and anger are true then it'll print out "Hangry"
