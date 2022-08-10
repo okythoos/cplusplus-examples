142ad122d118271d62e017810e6f92828199625d
j h <98184939+okythoos@users.noreply.github.com>
2022-08-09 18:44:06 -0700

    Create example of ||.c

---
+++ example of ||.c
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+  
+  int day = 6;
+  
+  // Write the code below:
+  if (day == 6 || day == 7) {
+    
+    //if day is equal to 6 or 7 then print out "Weekend"
+    
+    std::cout << "Weekend!\n";
+    
+  }
+
+}
